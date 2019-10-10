2af2ab2d1f2cf9725fd720de70a10be45c52a50f
Andy Nguyen <apnguyen11@yahoo.com>
2019-10-10 15:49:40 -0500

    Successfully implemented local authentication using passport in a node.js application. Through this, learning how to connect MongoDB using Mongoose was also achieved

--- index.js
+++ index.js
@@ -250,3 +250,34 @@ app.get('/auth/facebook',
       function(req, res){
         res.redirect('/success');
     });
+
+    // PASSPORT LOCAL AUTHENTICATION
+
+    const LocalStrategy = require('passport-local').Strategy;
+
+    passport.use(new LocalStrategy(
+      function(username, password, done){
+        UserDetails.findOne({
+          username:username
+        }, function(err, user){
+          if(err){
+            return done(err);
+          }
+
+          if(!user){
+            return done(null, false);
+          }
+
+          if(user.password != password){
+            return done(null, false);
+          }
+          return done(null, user);
+        })
+      }
+    ))
+
+    app.post('/',
+      passport.authenticate('local', { failureRedirect: '/error'}),
+      function(req, res){
+        res.redirect('/success?username='+req.user.username);
+      });
